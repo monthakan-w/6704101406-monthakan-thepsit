70efe78d455a04d20f68b8d5ffe332e63396027f
monthakan-w <Monthakan4386@gmail.com>
2025-10-08 10:00:47 +0700

    CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -35,7 +35,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/6704101406-monthakan-thepsit/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
